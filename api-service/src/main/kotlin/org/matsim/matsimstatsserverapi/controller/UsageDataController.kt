98278b87637d96ea4482d816e6e09fe353111b96
Thibaut Dubernet <thibaut.dubernet@ivt.baug.ethz.ch>
2018-08-09 13:55:44 +0200

    add get for testing

--- api-service/src/main/kotlin/org/matsim/matsimstatsserverapi/controller/UsageDataController.kt
+++ api-service/src/main/kotlin/org/matsim/matsimstatsserverapi/controller/UsageDataController.kt
@@ -3,10 +3,7 @@ package org.matsim.matsimstatsserverapi.controller
 import org.matsim.matsimstatsserverapi.service.StatsService
 import org.matsim.usagestats.UsageStats
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 /**
  * @author thibautd
@@ -24,5 +21,8 @@ class UsageDataController {
         // TODO: check what happens if request body invalid (and how to configure behavior)
     }
 
-    // TODO: add get requests. See if possible to restrict access
+    // TODO: restrict access?
+    @GetMapping("/data")
+    fun getData(): List<UsageStats> = statsService.allEntries()
+
 }
