8146c2e13b89d026ad94a790b763c04cc2663f7b
Thibaut Dubernet <thibaut.dubernet@ivt.baug.ethz.ch>
2018-08-13 13:09:09 +0200

    start Dashboard

--- dash/src/app.py
+++ dash/src/app.py
@@ -1,29 +1,81 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas.io.sql as psql
+import plotly.graph_objs as go
+import psycopg2 as pg
+from dash.dependencies import Output, Input
+from flask_caching import Cache
+import os
 
 app = dash.Dash()
 
+# Setup Redis caching.
+cache = Cache()
+CACHE_CONFIG = {
+    'CACHE_TYPE': 'redis',
+    # Keep data cached for 5 minutes. This basically corresponds to the time after which changes in the database
+    # become available (at the cost of more expensive queries)
+    # Adding an "invalidate cache" button would be possible, but would make the code messier for a rather limited need...
+    # For testing timeout can be reduced using the environment variable (a few seconds allow at least to only compute
+    # dataset once for several graphs at filter update
+    'CACHE_DEFAULT_TIMEOUT': os.getenv('REDIS_CACHE_DEFAULT_TIMEOUT', 300),
+    'CACHE_KEY_PREFIX': 'dash_',
+    'CACHE_REDIS_HOST': 'redis',
+    'CACHE_REDIS_PORT': 6379,
+    'CACHE_REDIS_DB': 1,
+    'CACHE_REDIS_URL': 'redis://redis:6379/1'}
+cache.init_app(app.server, config=CACHE_CONFIG)
+
+
+@cache.memoize()
+def global_store():
+    """Gets the data, possibly filtered by the parameters.
+       Data is cached using Redis, avoiding to recompute already known data as much as possible."""
+    connection = pg.connect(host="postgres", database="matsim_stats_db",
+                            user="postgres", password="password")
+
+    return psql.read_sql("SELECT * FROM usage_stats", connection)
+
+
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='MATSim Usage Statistics Dashboard'),
 
     html.Div(children='''
-        Dash: A web application framework for Python.
+        Summary of data collected about MATSim usage. Have fun!
     '''),
 
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    )
+    dcc.Dropdown(
+        id='dropdown',
+        options=[{'label': i, 'value': i} for i in global_store().os_name.unique().tolist()],
+        value='a'
+    ),
+
+    dcc.Graph(id='memory-graph'),
+
+    # hidden divs used to "signal" data changes to callbacks
+    # They store value of various dropdowns etc, but provide them only after data was queried and cached.
+    html.Div(id='signal', style={'display': 'none'})
 ])
+
+
+@app.callback(Output('signal', 'children'), [Input('dropdown', 'value')])
+def compute_value(value):
+    # compute value and send a signal when done
+    global_store()
+    return value
+
+
+@app.callback(Output('memory-graph', 'figure'),
+              [Input('signal', 'children')])
+def memory_graph(value):
+    d = global_store()
+    return go.Scatter(
+        x=d.populationSize,
+        y=d.peakHeapMB,
+        mode='markers'
+    )
+
 
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0', debug=True)
