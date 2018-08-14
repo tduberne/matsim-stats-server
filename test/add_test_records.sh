33c7affac36165c7678e8df9915b778945e43c00
Thibaut Dubernet <thibaut.dubernet@ivt.baug.ethz.ch>
2018-08-14 17:32:38 +0200

    remove port number

--- test/add_test_records.sh
+++ test/add_test_records.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 for f in records/*.json; do
-	curl -H "Content-Type: application/json" --data "@$f" http://localhost:8000/api/data
+	curl -H "Content-Type: application/json" --data "@$f" http://localhost/api/data
 done
